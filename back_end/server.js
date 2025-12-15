4797d71f3e8eb790aeef39070f08de12090101e5
manishsuthar01 <manishsuthar78775@gmail.com>
2025-12-15 15:22:29 +0530

    Deployment

--- back_end/server.js
+++ back_end/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
@@ -17,14 +18,18 @@ dotenv.config();
 app.use(express.json()); // this is neede to read the data from body , to parse the incoming requests with JSON paylaod
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Server is working!");
-});
+// used to serve static files such as html ,css images etc from the frontend
+app.use(express.static(path.join(__dirname, "../front_end/dist")));
 
 app.use("/api/auth/", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../front_end/dist/index.html"));
+});
 server.listen(PORT, () => {
   connectTomongoDB();
   console.log(`server is running at prot ${PORT}`);
